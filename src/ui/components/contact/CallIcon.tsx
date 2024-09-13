1f813bb5dab3989aaf6c96625fa0519fa89e9828
dungtutb <phamdunghc1@gmail.com>
2024-09-13 22:53:40 +0700

    add icons

--- src/ui/components/contact/CallIcon.tsx
+++ src/ui/components/contact/CallIcon.tsx
@@ -5,7 +5,7 @@ import styles from './Icon.module.css'; // Import CSS
 export const CallIcon = () => {
   return (
     <div className={styles['call-icon']}>
-      <Link href="tel:+0348822238" target="_blank">
+      <Link href="tel:+84348822238" target="_blank">
           <Image 
             src="/call.svg" 
             alt="Call" 
